a282b303ecd640cc7d24f19c084e83554cd9db8d
Mick Sayson <mick@sayson.com>
2021-12-30 17:50:50 -0800

    Windows build support
    * Patch deepspeech-sys with a custom version that requires no
      configuration on both windows and linux
        * On windows the deepspeech library is shipped as libdeepspeech.so,
          but is actually a DLL. AFAICT there's no way to give rustc an
          absolute path to a library. -l will not find a .so under windows
          so we need a .dll. At runtime the app expects the original
          filename. This means we need both the dll and the so. Symlink one
          to the other
        * Copy the runtime objects to OUT_DIR and use that as our search
          path. Cargo will append linker paths in OUT_DIR to
          LD_LIBRARY_PATH/PATH allowing us to run without any configuration
    * Fix flite-sys build. Build everything we see in a make --dry-run

---
+++ deepspeech-sys/build.rs
@@ -0,0 +1,55 @@
+use std::env;
+use std::path::{Path, PathBuf};
+
+
+fn copy_file(src: &Path, dst: &Path) {
+    if dst.exists() {
+        std::fs::remove_file(&dst).unwrap();
+    }
+
+    std::fs::copy(src, dst).unwrap();
+}
+
+#[cfg(target_os = "linux")]
+fn copy_lib_to_out_dir(manifest_dir: &Path, out_dir: &Path) {
+    copy_file(&manifest_dir.join("res/linux/libdeepspeech.so"), &out_dir.join("libdeepspeech.so"));
+}
+
+#[cfg(target_os = "windows")]
+fn copy_lib_to_out_dir(manifest_dir: &Path, out_dir: &Path) {
+    // Need the .so at runtime and the .dll at link time. Rust can only use -l
+    // flags to link the so, and it won't find lib*.so on windows. Unfortunately
+    // at runtime we still expect to find the .so even if we linked against the
+    // renamed dll. 
+    let output_so = out_dir.join("libdeepspeech.so");
+    let output_dll = out_dir.join("libdeepspeech.dll");
+    copy_file(
+        &manifest_dir.join("res/windows/libdeepspeech.so"),
+        &output_so,
+    );
+    if output_dll.exists() {
+        std::fs::remove_file(&output_dll).unwrap();
+    }
+    std::fs::hard_link(output_so, output_dll).unwrap();
+}
+
+fn main() {
+    let out_dir =env::var("OUT_DIR").unwrap();
+
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    println!("cargo:rustc-link-lib=dylib=deepspeech");
+
+    let bindings = bindgen::builder()
+        .header(&manifest_dir.join("res/deepspeech.h").display().to_string())
+        .generate()
+        .unwrap();
+
+    bindings.write_to_file(format!("{}/bindings.rs", out_dir)).unwrap();
+
+    // Cargo will append link search paths to LD_LIBRARY_PATH/PATH, but only if
+    // they're in OUT_DIR. Copy the lib there so that cargo run works with no
+    // configuration
+    copy_lib_to_out_dir(&manifest_dir, &PathBuf::from(&out_dir));
+    println!("cargo:rustc-link-search={}", out_dir);
+
+}
--- flite-sys/build.rs
+++ flite-sys/build.rs
@@ -1,104 +1,184 @@
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::env;
 
 // Subset of files to give us the ability to call flite_text_to_wave and
 // cst_wave_resample
-const FILE_LIST: [&str; 48] = [
-    "lang/cmulex/cmu_lex.c",
-    "lang/cmulex/cmu_lex_data.c",
-    "lang/cmulex/cmu_lts_model.c",
-    "lang/cmulex/cmu_lts_rules.c",
-    "lang/cmulex/cmu_lex_entries.c",
-    "lang/cmulex/cmu_postlex.c",
+const FILE_LIST: [&str; 144] = [
+    "src/audio/auclient.c",
+    "src/audio/auserver.c",
+    "src/audio/audio.c",
     "src/audio/au_streaming.c",
-    "src/cg/cst_cg.c",
-    "src/cg/cst_spamf0.c",
-    "src/cg/cst_mlsa.c",
-    "src/cg/cst_mlpg.c",
-    "src/cg/cst_cg_map.c",
-    "src/cg/cst_cg_load_voice.c",
-    "src/cg/cst_vc.c",
-    "src/hrg/cst_ffeature.c",
-    "src/hrg/cst_item.c",
-    "src/hrg/cst_relation.c",
-    "src/hrg/cst_utterance.c",
-    "src/lexicon/cst_lexicon.c",
-    "src/lexicon/cst_lts.c",
-    "src/regex/cst_regex.c",
-    "src/regex/regexp.c",
-    "src/stats/cst_cart.c",
-    "src/speech/cst_track.c",
-    "src/speech/cst_wave.c",
-    "src/speech/cst_wave_io.c",
-    "src/speech/cst_lpcres.c",
-    "src/speech/rateconv.c",
-    "src/synth/cst_synth.c",
-    "src/synth/cst_ffeatures.c",
-    "src/synth/cst_phoneset.c",
-    "src/synth/cst_voice.c",
-    "src/synth/flite.c",
-    "src/synth/cst_utt_utils.c",
+    "src/audio/au_none.c",
     "src/utils/cst_alloc.c",
     "src/utils/cst_error.c",
     "src/utils/cst_string.c",
-    "src/utils/cst_features.c",
-    "src/utils/cst_file_stdio.c",
+    "src/utils/cst_wchar.c",
     "src/utils/cst_tokenstream.c",
     "src/utils/cst_val.c",
-    "src/utils/cst_val_user.c",
-    "src/utils/cst_val_const.c",
+    "src/utils/cst_features.c",
     "src/utils/cst_endian.c",
-    "src/utils/cst_url.c",
     "src/utils/cst_socket.c",
-    "src/wavesynth/cst_clunits.c",
-    // We are not reading/writing files through flite in our rust bindings, so
-    // having an unimplemented mmap that's common across all platforms should be
-    // fine. If we find in the future that this is causing problems we can
-    // detect the target os and build the appropriate files
+    "src/utils/cst_val_const.c",
+    "src/utils/cst_val_user.c",
+    "src/utils/cst_args.c",
+    "src/utils/cst_url.c",
     "src/utils/cst_mmap_none.c",
+    "src/utils/cst_file_stdio.c",
+    "src/regex/cst_regex.c",
+    "src/regex/regexp.c",
+    "src/regex/regsub.c",
+    "src/hrg/cst_utterance.c",
+    "src/hrg/cst_relation.c",
+    "src/hrg/cst_item.c",
+    "src/hrg/cst_ffeature.c",
+    "src/hrg/cst_rel_io.c",
+    "src/stats/cst_cart.c",
+    "src/stats/cst_viterbi.c",
+    "src/stats/cst_ss.c",
+    "src/speech/cst_wave.c",
+    "src/speech/cst_wave_io.c",
+    "src/speech/cst_track.c",
+    "src/speech/cst_track_io.c",
+    "src/speech/cst_wave_utils.c",
+    "src/speech/cst_lpcres.c",
+    "src/speech/rateconv.c",
+    "src/speech/g721.c",
+    "src/speech/g72x.c",
+    "src/speech/g723_24.c",
+    "src/speech/g723_40.c",
+    "src/lexicon/cst_lexicon.c",
+    "src/lexicon/cst_lts.c",
+    "src/lexicon/cst_lts_rewrites.c",
+    "src/synth/cst_synth.c",
+    "src/synth/cst_utt_utils.c",
+    "src/synth/cst_voice.c",
+    "src/synth/cst_phoneset.c",
+    "src/synth/cst_ffeatures.c",
+    "src/synth/cst_ssml.c",
+    "src/synth/flite.c",
+    "src/wavesynth/cst_units.c",
+    "src/wavesynth/cst_clunits.c",
+    "src/wavesynth/cst_diphone.c",
+    "src/wavesynth/cst_sigpr.c",
+    "src/wavesynth/cst_sts.c",
+    "src/wavesynth/cst_reflpc.c",
+    "src/cg/cst_cg.c",
+    "src/cg/cst_mlsa.c",
+    "src/cg/cst_mlpg.c",
+    "src/cg/cst_vc.c",
+    "src/cg/cst_cg_load_voice.c",
+    "src/cg/cst_cg_dump_voice.c",
+    "src/cg/cst_cg_map.c",
+    "src/cg/cst_spamf0.c",
+    "lang/cmulex/cmu_lts_rules.c",
+    "lang/cmulex/cmu_lts_model.c",
+    "lang/cmulex/cmu_lex.c",
+    "lang/cmulex/cmu_lex_entries.c",
+    "lang/cmulex/cmu_lex_data.c",
+    "lang/cmulex/cmu_postlex.c",
+    "lang/cmu_indic_lex/cmu_indic_lex.c",
+    "lang/cmu_grapheme_lex/cmu_grapheme_lex.c",
+    "lang/cmu_grapheme_lex/grapheme_unitran_tables.c",
+    "lang/usenglish/us_int_accent_cart.c",
+    "lang/usenglish/us_int_tone_cart.c",
+    "lang/usenglish/us_f0_model.c",
+    "lang/usenglish/us_dur_stats.c",
+    "lang/usenglish/us_durz_cart.c",
+    "lang/usenglish/us_f0lr.c",
+    "lang/usenglish/us_phoneset.c",
+    "lang/usenglish/us_ffeatures.c",
+    "lang/usenglish/us_phrasing_cart.c",
+    "lang/usenglish/us_gpos.c",
+    "lang/usenglish/us_text.c",
+    "lang/usenglish/us_expand.c",
+    "lang/usenglish/us_nums_cart.c",
+    "lang/usenglish/us_aswd.c",
+    "lang/usenglish/usenglish.c",
+    "lang/usenglish/us_pos_cart.c",
+    "lang/cmu_indic_lang/cmu_indic_lang.c",
+    "lang/cmu_indic_lang/cmu_indic_phoneset.c",
+    "lang/cmu_indic_lang/cmu_indic_phrasing_cart.c",
+    "lang/cmu_grapheme_lang/cmu_grapheme_lang.c",
+    "lang/cmu_grapheme_lang/cmu_grapheme_phoneset.c",
+    "lang/cmu_grapheme_lang/cmu_grapheme_phrasing_cart.c",
+    "lang/cmu_us_kal/cmu_us_kal_diphone.c",
+    "lang/cmu_us_kal/cmu_us_kal.c",
+    "lang/cmu_us_kal/cmu_us_kal_lpc.c",
+    "lang/cmu_us_kal/cmu_us_kal_res.c",
+    "lang/cmu_us_kal/cmu_us_kal_residx.c",
+    "lang/cmu_us_kal/cmu_us_kal_ressize.c",
+    "lang/cmu_time_awb/cmu_time_awb.c",
+    "lang/cmu_time_awb/cmu_time_awb_clunits.c",
+    "lang/cmu_time_awb/cmu_time_awb_cart.c",
+    "lang/cmu_time_awb/cmu_time_awb_mcep.c",
+    "lang/cmu_time_awb/cmu_time_awb_lpc.c",
+    "lang/cmu_time_awb/cmu_time_awb_lex_entry.c",
+    "lang/cmu_us_kal16/cmu_us_kal16_diphone.c",
+    "lang/cmu_us_kal16/cmu_us_kal16.c",
+    "lang/cmu_us_kal16/cmu_us_kal16_lpc.c",
+    "lang/cmu_us_kal16/cmu_us_kal16_res.c",
+    "lang/cmu_us_kal16/cmu_us_kal16_residx.c",
+    "lang/cmu_us_awb/cmu_us_awb.c",
+    "lang/cmu_us_awb/cmu_us_awb_cg_single_mcep_trees.c",
+    "lang/cmu_us_awb/cmu_us_awb_cg.c",
+    "lang/cmu_us_awb/cmu_us_awb_cg_single_params.c",
+    "lang/cmu_us_awb/cmu_us_awb_cg_durmodel.c",
+    "lang/cmu_us_awb/cmu_us_awb_cg_phonestate.c",
+    "lang/cmu_us_awb/cmu_us_awb_cg_f0_trees.c",
+    "lang/cmu_us_awb/cmu_us_awb_spamf0_accent_params.c",
+    "lang/cmu_us_awb/cmu_us_awb_spamf0_phrase.c",
+    "lang/cmu_us_awb/cmu_us_awb_spamf0_accent.c",
+    "lang/cmu_us_rms/cmu_us_rms.c",
+    "lang/cmu_us_rms/cmu_us_rms_cg_single_mcep_trees.c",
+    "lang/cmu_us_rms/cmu_us_rms_cg.c",
+    "lang/cmu_us_rms/cmu_us_rms_cg_single_params.c",
+    "lang/cmu_us_rms/cmu_us_rms_cg_durmodel.c",
+    "lang/cmu_us_rms/cmu_us_rms_cg_phonestate.c",
+    "lang/cmu_us_rms/cmu_us_rms_cg_f0_trees.c",
+    "lang/cmu_us_rms/cmu_us_rms_spamf0_accent_params.c",
+    "lang/cmu_us_rms/cmu_us_rms_spamf0_phrase.c",
+    "lang/cmu_us_rms/cmu_us_rms_spamf0_accent.c",
+    "lang/cmu_us_slt/cmu_us_slt.c",
+    "lang/cmu_us_slt/cmu_us_slt_cg_single_mcep_trees.c",
+    "lang/cmu_us_slt/cmu_us_slt_cg.c",
+    "lang/cmu_us_slt/cmu_us_slt_cg_single_params.c",
+    "lang/cmu_us_slt/cmu_us_slt_cg_durmodel.c",
+    "lang/cmu_us_slt/cmu_us_slt_cg_phonestate.c",
+    "lang/cmu_us_slt/cmu_us_slt_cg_f0_trees.c",
+    "lang/cmu_us_slt/cmu_us_slt_spamf0_accent.c",
+    "lang/cmu_us_slt/cmu_us_slt_spamf0_phrase.c",
+    "lang/cmu_us_slt/cmu_us_slt_spamf0_accent_params.c",
 ];
-
-fn find_c_files(path: &Path) -> Vec<PathBuf> {
-
-    let mut ret = Vec::new();
-
-    for entry in path.read_dir().expect("Failed to read path") {
-        let entry = entry.expect("Invalid entry");
-        let entry_path = entry.path();
-        if entry.metadata().unwrap().is_dir() {
-            ret.extend(find_c_files(&entry_path));
-        }
-        else if let Some(ext) = entry_path.extension() {
-            if ext == "c" {
-                ret.push(entry_path);
-            }
-        }
-    }
-
-    return ret;
-}
 
 fn main() {
     let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     let vendor_path = manifest_dir.join("vendor");
 
-    println!("{:?}", find_c_files(&vendor_path.join("src")));
-
-    cc::Build::new()
+    let mut builder = cc::Build::new();
+    builder
         .files(FILE_LIST.iter().map(|&p| vendor_path.join(p)).collect::<Vec<_>>())
-        .files(find_c_files(&vendor_path.join("lang/cmu_us_rms")))
-        .files(find_c_files(&vendor_path.join("lang/usenglish")))
         .file(manifest_dir.join("src/flite_voice_list.c"))
         .file(manifest_dir.join("src/flite_lang_list.c"))
         .include(vendor_path.join("include"))
         .include(vendor_path.join("lang/usenglish"))
         .include(vendor_path.join("lang/cmulex"))
-        .warnings(false)
-        .compile("flite");
+        .opt_level(2)
+        .define("CST_NO_SOCKETS", None)
+        .define("CST_AUDIO_NONE", None)
+        .warnings(false);
+
+    if cfg!(target_os = "windows") {
+        builder
+            .define("UNDER_WINDOWS", None)
+            .define("WIN32", None);
+    }
+
+    builder.compile("flite");
 
     let bindings = bindgen::builder()
         .clang_arg(format!("-I{}", vendor_path.join("include").to_string_lossy()))
         .header(manifest_dir.join("bindings.h").to_string_lossy())
+        .blocklist_item("_JUMP_BUFFER")
         .generate();
 
     let out_dir = env::var("OUT_DIR").unwrap();
